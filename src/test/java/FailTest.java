61b77212ff9bc607f11281403cf7f1e999accb41
Nehan <ans2ur@virginia.edu>
2018-09-13 00:22:39 -0400

    Updated project

---
+++ src/test/java/FailTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+
+public class FailTest {
+
+        @Test
+        public void this_should_pass(){
+            assertTrue(false);
+        }
+
+        @Test
+        public void this_should_pass_too(){
+            assertTrue(false);
+        }
+
+}
